e9991b48a9bb68b489a68e1dd204b9719de4e42a
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-23 18:23:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
+function getIt(){
+  alert('Hey!');
+}
+
+function frameIt(){
+  $('img').addClass('tasty');
+}
+
+function pressIt(){
+  alert('you press G');
+}
+
 $(document).ready(function(){
 
 // call functions here
+  $('p').on('click', function(){
+    getIt();
+  });
+
+  $('img').on('load', function(){
+    frameIt();
+  });
+
+  $('#typing').on('keydown', function(e){
+    if(e.which == 71){
+      pressIt();
+    }
+  });
 
 });
